9e0d89cbfd9b16b981180a556b13d69b0fcc6297
Sudhanshu Arya <aryasudhanshu@gmail.com>
2019-06-20 13:57:01 +0530

    Boxes added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Box from './Box';
+import BoxList from './BoxList';
 
 function App() {
   return (
     <div>
-      <Box height={10} width={20} color="purple" />
-      <Box height={20} width={15} color="teal" />
+      <BoxList />
     </div>
   );
 }
--- src/Box.js
+++ src/Box.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 class Box extends Component {
-  state = {  }
   render() { 
     return ( 
       <div>
