c10b38be0936594e7563818445db730bc6773e39
Sudhanshu Arya <aryasudhanshu@gmail.com>
2019-06-20 12:07:07 +0530

    Box renders well

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Box from './Box';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Box height={10} width={20} color="purple" />
+      <Box height={20} width={15} color="teal" />
     </div>
   );
 }
